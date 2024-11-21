59e6fabd611cd3ce7dbd4766daeb1cbdd1c37432
Donghyun Lee <leedonghyun022@gmail.com>
2024-11-21 16:15:04 +0900

    feat: 클라이언트 나갔을때 삭제 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,20 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
+
 import "./index.css";
 import { App } from "./App.tsx";
+import { ToastList, ToastRegion } from "./ui/base/Toaster.tsx";
 
 const root = document.getElementById("root");
 
-render(() => <App />, root!);
+render(
+  () => (
+    <>
+      <App />
+      <ToastRegion>
+        <ToastList />
+      </ToastRegion>
+    </>
+  ),
+  root!
+);
--- src/ui/screen/Monitor.tsx
+++ src/ui/screen/Monitor.tsx
@@ -1,11 +1,32 @@
+import { toaster } from "@kobalte/core";
 import { createSignal, For, onMount } from "solid-js";
 
 import { token } from "../../service/signal/token";
+import {
+  Toast,
+  ToastContent,
+  ToastProgress,
+  ToastTitle,
+} from "../base/Toaster";
 
 type Client = {
   name: string;
   uri: string;
 };
+
+type Message =
+  | {
+      client: Client;
+      type: "CLINET_DATA";
+    }
+  | {
+      name: string;
+      type: "ALERT";
+    }
+  | {
+      name: string;
+      type: "CLIENT_EXIT";
+    };
 
 const Video = (props: Client) => {
   return (
@@ -15,7 +36,7 @@ const Video = (props: Client) => {
         playsinline
         src={`${import.meta.env.VITE_APP_API_URL}/${props.uri}`}
       />
-      <p>{props.name}</p>
+      <p class="font-mono font-medium">{props.name}</p>
     </div>
   );
 };
@@ -36,18 +57,35 @@ export const Monitor = () => {
       console.log("WebSocket connection closed");
     };
     ws.onmessage = (event) => {
-      const client = JSON.parse(event.data) as Client;
-      if (clients().some((c) => c.name === client.name)) {
-        setClients((prev) =>
-          prev.map((c) => (c.name === client.name ? client : c))
-        );
-      } else {
-        setClients((prev) => [...prev, client]);
+      const data = JSON.parse(event.data) as Message;
+      switch (data.type) {
+        case "ALERT":
+          toaster.show((props) => (
+            <Toast toastId={props.toastId}>
+              <ToastContent>
+                <ToastTitle>Toast</ToastTitle>
+              </ToastContent>
+              <ToastProgress />
+            </Toast>
+          ));
+          return;
+        case "CLIENT_EXIT":
+          setClients((prev) => prev.filter((c) => c.name !== data.name));
+          return;
+        case "CLINET_DATA":
+          if (clients().some((c) => c.name === data.client.name)) {
+            setClients((prev) =>
+              prev.map((c) => (c.name === data.client.name ? data.client : c))
+            );
+          } else {
+            setClients((prev) => [...prev, data.client]);
+          }
+          return;
       }
     };
   });
   return (
-    <div class="grid-cols-3 grid">
+    <div class="grid-cols-3 grid gap-5">
       <For each={clients()}>
         {(client) => <Video name={client.name} uri={client.uri} />}
       </For>
